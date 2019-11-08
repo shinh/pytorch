d857fe6d27b987e9662e3403e2588cbf7b3acf03
Brian Vaughan <bvaughan@fb.com>
2019-11-08 15:14:38 -0500

    Fix bug in atomicAdd for int16_t
    ghstack-source-id: 56d028fa7a2983185160590d430b285fecceca5b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/29231

--- test/test_torch.py
+++ test/test_torch.py
@@ -13395,6 +13395,25 @@ class TestTorchDeviceType(TestCase):
         result = torch.cat(concat_list)
         self.assertEqual(result.size(0), SIZE1 + SIZE2)
 
+    @onlyCUDA
+    def test_atomic_add(self, device):
+        # https://github.com/pytorch/pytorch/issues/29475
+        def is_signed(dtype):
+            return torch.is_signed(torch.tensor(0, dtype=dtype))
+        for dtype in torch.testing.get_all_math_dtypes(device):
+            size = [5, 5]
+            if dtype.is_floating_point:
+                tensor = torch.rand(size, dtype=dtype, device=device)
+            elif is_signed(dtype):
+                tensor = torch.randint(-5, 15, size, dtype=dtype, device=device)
+            else:
+                tensor = torch.randint(0, 10, size, dtype=dtype, device=device)
+
+            # index_add calls atomicAdd on cuda.
+            empty = torch.zeros(size, dtype=dtype, device=device)
+            added = empty.index_add(0, torch.arange(0, size[0], dtype=torch.long, device=device), tensor)
+            self.assertEqual(added, tensor)
+
 
 # Tests that compare a device's computation with the (gold-standard) CPU's.
 class TestDevicePrecision(TestCase):
